bbfbb83867abc65d39fd56238e26024c9652ff40
Steven Levis <levis501@yahoo.com>
2018-06-12 20:01:24 -0700

    fixes syntax error

--- client/src/layout/resource-links/ResourceLinksPage.js
+++ client/src/layout/resource-links/ResourceLinksPage.js
@@ -24,7 +24,7 @@ const Row = (row, filter) => {
       </Table.Cell>
       <Table.Cell>
         {/* Don't display the tag associated with the filter, since it is redundant */}
-        {row.tags.map((t) => (t === filter) ? null : (<Label className='tableCellLabel'>{t}</Label>))}
+        {row.tags.map((t) => (t === filter) ? null : (<Label class='tableCellLabel'>{t}</Label>))}
       </Table.Cell>
     </Table.Row >
   )
