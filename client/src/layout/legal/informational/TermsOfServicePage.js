9b5137d866605236fd86e223abea0015e9b55e9f
Leah Sandacz <sandas@outlook.com>
2018-06-24 13:40:20 -0700

    Move legal text into their own files for reuse

--- client/src/layout/legal/informational/TermsOfServicePage.js
+++ client/src/layout/legal/informational/TermsOfServicePage.js
@@ -6,22 +6,12 @@ import {
 import PageLayout from '../../components/PageLayout';
 import LegalTextContainer from './LegalTextContainer';
 
+import TermsOfServiceText from '../text/TermsOfServiceText';
+
 const TermsOfServicePage = () => (
   <PageLayout>
     <LegalTextContainer>
-      <Header as='h1'>Terms of Service</Header>
-      <Header as='h2'>Section</Header>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      </p>
-      <Header as='h2'>Section</Header>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      </p>
-      <Header as='h2'>Section</Header>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      </p>
+      <TermsOfServiceText/>
     </LegalTextContainer>
   </PageLayout>
 )
