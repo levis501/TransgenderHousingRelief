9369d3dd49f6e5ed95dcbf9c6423a96db375e26c
Leah Sandacz <sandas@outlook.com>
2018-06-23 18:23:33 -0700

    Contact page

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,13 @@ import HomePage from './layout/homepage/HomePage';
 import SearchPage from './layout/search/SearchPage';
 import SubmitAdPage from './layout/submitAd/SubmitAdPage';
 import ResourceLinksPage from './layout/resource-links/ResourceLinksPage';
-import FeedbackPage from './layout/feedback/FeedbackPage';
 import FaqPage from './layout/faq/FaqPage';
 import LoginPage from './layout/login/LoginPage';
 import ForgotUsernamePage from './layout/login/ForgotUsernamePage';
 import ForgotPasswordPage from './layout/login/ForgotPasswordPage';
 import RegistrationPage from './layout/register/RegistrationPage';
+import FeedbackPage from './layout/feedback/FeedbackPage';
+import ContactPage from './layout/contact/ContactPage';
 
 class App extends Component {
   render() {
@@ -23,11 +24,12 @@ class App extends Component {
           <Route path='/faq' component={FaqPage}/>
           <Route path='/legal' render={() => (<ResourceLinksPage select={/legal-help/} title='Legal Resources' />)} />
           <Route path='/housing-resources' render={() => (<ResourceLinksPage select={/housing/} skip={/^legal-.+/} title='Housing Resources' />)} />
-          <Route path='/feedback' component={FeedbackPage}/>
           <Route path='/login/forgot-username' component={ForgotUsernamePage}/>
           <Route path='/login/forgot-password' component={ForgotPasswordPage}/>
           <Route path='/login' component={LoginPage}/>
           <Route path='/signup' component={RegistrationPage}/>
+          <Route path='/feedback' component={FeedbackPage}/>
+          <Route path='/contact' component={ContactPage}/>
         </Switch>
       </Router>
     );
---
+++ client/src/layout/contact/ContactPage.js
@@ -0,0 +1,98 @@
+import React, { Component } from 'react';
+import {
+  Button,
+  Container,
+  Dropdown,
+  Form,
+  Grid,
+  Header,
+  Input
+} from 'semantic-ui-react';
+
+import PageLayout from '../components/PageLayout';
+import TextAreaWithLimit from '../../components/forms/TextAreaWithLimit';
+
+class ContactForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      email: '',
+      subject: '',
+      body: '',
+      bodyOverLimit: false
+    }
+    this.onChangeName = this.onChangeName.bind(this);
+    this.onChangeEmail = this.onChangeEmail.bind(this);
+    this.onChangeSubjectText = this.onChangeSubjectText.bind(this);
+    this.onChangeBodyText = this.onChangeBodyText.bind(this);
+  }
+  onChangeName(e, props) {
+    this.setState({ name: props.value });
+  }
+  onChangeEmail(e, props) {
+    this.setState({ email: props.value });
+  }
+  onChangeSubjectText(e, props) {
+    this.setState({ subject: props.value });
+  }
+  onChangeBodyText(body, bodyOverLimit) {
+    this.setState({ body, bodyOverLimit });
+  }
+  render() {
+    return (
+      <Form>
+        <Form.Field>
+          <label>Name</label>
+          <Input
+            value={this.state.name}
+            onChange={this.onChangeName}
+            />
+        </Form.Field>
+        <Form.Field>
+          <label>Email</label>
+          <Input
+            value={this.state.email}
+            onChange={this.onChangeEmail}
+            />
+        </Form.Field>
+        <Form.Field>
+          <label>Subject</label>
+          <Input
+            value={this.state.subject}
+            onChange={this.onChangeSubjectText}
+            />
+        </Form.Field>
+        <Form.Field>
+          <TextAreaWithLimit
+            value={this.state.body}
+            onChange={this.onChangeBodyText}
+            />
+        </Form.Field>
+        <Grid>
+          <Grid.Row centered>
+            <Grid.Column textAlign='center'>
+              <Button primary size='large'
+                disabled={this.state.bodyOverLimit}
+                >
+                Submit
+              </Button>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Form>
+    );
+  }
+}
+
+const ContactPage = () => (
+  <PageLayout>
+    <Container>
+      <Header as='h1'>Contact</Header>
+      <ContactForm/>
+    </Container>
+  </PageLayout>
+)
+
+
+export default ContactPage;
