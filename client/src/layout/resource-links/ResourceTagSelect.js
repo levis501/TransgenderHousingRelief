8bc4a7d825e1030e2e72605c913d6758150aff30
Steven Levis <levis501@yahoo.com>
2018-06-24 14:04:50 -0700

    [WIP]add custom search method

--- client/src/layout/resource-links/ResourceTagSelect.js
+++ client/src/layout/resource-links/ResourceTagSelect.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Dropdown, Form, Button } from 'semantic-ui-react';
+import escapeStringRegexp from 'escape-string-regexp';
 
 class ResourceTagSelect extends Component {
   constructor(props) {
@@ -8,29 +9,37 @@ class ResourceTagSelect extends Component {
       tags: []
     }
     this.onChangeTags = this.onChangeTags.bind(this);
-    this.dropdownStyle = { 'minWidth': '300px' }
-    this.formStyle = { '': '10px' }
   }
+
   onTagClicked(tag) {
     if (this.state.tags.includes(tag)) return;
     this.setState({ tags: [...this.state.tags, tag] })
   }
+
   onChangeTags(e, { value }) {
     this.setState({ tags: value });
     this.props.onChangeTags(value);
   }
+
   clearFilter() {
     this.setState({ tags: [] });
     this.props.onChangeTags([]);
   }
+
+  search(options, input) {
+    const minify = (str) => str.replace(/(\s|-)/g, '');
+    const inputRegex = new RegExp(escapeStringRegexp(minify(input)),'i');
+    return options.filter((option) => minify(option.value).match(inputRegex));
+  }
+
   render() {
     return (
       <Form>
         <Form.Input label='Filter: '>
           <Dropdown fluid selection search multiple placeholder='Select tags...'
-            // style={this.dropdownStyle}
             options={this.props.allTags.map((item) => ({ text: item, value: item }))}
             value={this.state.tags} onChange={this.onChangeTags}
+            search={this.search.bind(this)}
           />
         </Form.Input>
         {(this.state.tags.length < 2) ? null :
