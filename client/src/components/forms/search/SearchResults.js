772604dfb46399fe11a201e54f6d37672e53a3fc
Leah Sandacz <sandas@outlook.com>
2018-08-06 14:02:49 -0700

    Use customized pagination control

--- client/src/components/forms/search/SearchResults.js
+++ client/src/components/forms/search/SearchResults.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import { Grid, Item, Pagination, Header, Icon } from 'semantic-ui-react';
+import {
+  Grid,
+  Item,
+  Header
+} from 'semantic-ui-react';
 
+import Pagination from '../../Pagination';
 import SortBy from './SortBy';
-
 import SearchResult from './SearchResult';
 
 const SearchResults = ({showResultsNumber}) => (
@@ -47,11 +51,6 @@ const SearchResults = ({showResultsNumber}) => (
       <Grid.Column textAlign='center'>
         <Pagination
           defaultActivePage={1}
-          ellipsisItem={{ content: <Icon name='ellipsis horizontal' />, icon: true }}
-          firstItem={{ content: <Icon name='angle double left' />, icon: true }}
-          lastItem={{ content: <Icon name='angle double right' />, icon: true }}
-          prevItem={{ content: <Icon name='angle left' />, icon: true }}
-          nextItem={{ content: <Icon name='angle right' />, icon: true }}
           totalPages={3}
         />
       </Grid.Column>
