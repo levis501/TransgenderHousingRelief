e08e1c51230956d44755dd8d7a5a8ba0a31a2994
Leah Sandacz <sandas@outlook.com>
2018-08-06 12:51:26 -0700

    Responsive buttons for listing page

--- client/src/layout/listing/ListingMenu.js
+++ client/src/layout/listing/ListingMenu.js
@@ -2,10 +2,34 @@ import React from 'react';
 import {
   Button,
   Icon,
-  Grid
+  Grid,
+  Responsive
 } from 'semantic-ui-react';
 
-const ListingMenu = () => (
+const ListingMenuMobile = () => (
+  <Grid stackable>
+    <Grid.Row>
+      <Grid.Column width={8}>
+        <Button fluid color='blue'>
+          <Icon name='mail' /> Message
+        </Button>
+        <Button fluid color='pink'>
+          <Icon name='bookmark' /> Save
+        </Button>
+      </Grid.Column>
+      <Grid.Column width={8} textAlign='right'>
+        <Button fluid negative>
+          <Icon name='x' /> Delete Post
+        </Button>
+        <Button fluid basic negative>
+          <Icon name='exclamation circle' /> Report Post
+        </Button>
+      </Grid.Column>
+    </Grid.Row>
+  </Grid>
+)
+
+const ListingMenuDesktop = () => (
   <Grid>
     <Grid.Row>
       <Grid.Column width={8}>
@@ -26,6 +50,17 @@ const ListingMenu = () => (
       </Grid.Column>
     </Grid.Row>
   </Grid>
+)
+
+const ListingMenu = () => (
+  <React.Fragment>
+    <Responsive maxWidth={511}>
+      <ListingMenuMobile />
+    </Responsive>
+    <Responsive minWidth={512}>
+      <ListingMenuDesktop />
+    </Responsive>
+  </React.Fragment>
 )
 
 export default ListingMenu;
